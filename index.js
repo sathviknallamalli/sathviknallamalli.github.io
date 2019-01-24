dfdc6e1f64c5b981c156a4fbcba5f41f3d194ba8
sathv <sathviknallamalli@gmail.com>
2019-01-23 23:29:04 -0800

    facebook

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function send_verification() {
   });
 }
 
-function facebook_login() {
+function gmail_login() {
   var provider = new firebase.auth.GoogleAuthProvider();
 
   firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -108,4 +108,29 @@ function facebook_login() {
     window.alert("error mess + " + error.message + " user email " + email);
     // ...
   });
+}
+
+function facebook_login() {
+  var provider = new firebase.auth.FacebookAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+
+  window.alert("login with f " + user.email);
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+
+  window.alert("error message " + errorMessage);
+});
 }
