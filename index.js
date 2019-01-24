8b426e93ccfa206f1f39c996eae538d77ceb695f
sathv <sathviknallamalli@gmail.com>
2019-01-23 23:20:10 -0800

    login gui

--- index.js
+++ index.js
@@ -1,27 +1,111 @@
-
-
 firebase.auth().onAuthStateChanged(function(user) {
+
   if (user) {
     // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if (user != null) {
+
+      var email_id = user.email;
+      var email_verified = user.emailVerified;
+
+      if (email_verified) {
+        document.getElementById("verify_btn").style.display = "none"
+      } else {
+        document.getElementById("verify_btn").style.display = "block"
+      }
+
+
+
+      document.getElementById("user_para").innerHTML = "welcome user " + email_id +
+        "<br/>verified " + email_verified;
+
+
+
+    }
+
   } else {
     // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
   }
 });
 
-function weblogin(){
+function login() {
 
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
 
-  var useremail = document.getElementById("email").value;
-  var userpassword = document.getElementById("password").value;
-
-
-
-    firebase.auth().signInWithEmailAndPassword(useremail, userpassword).catch(function(error) {
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
 
-    window.alert("error: " + errorMessage);
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function create_account() {
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+}
+
+function logout() {
+  firebase.auth().signOut();
+}
+
+function send_verification() {
+  var user = firebase.auth().currentUser;
+
+  user.sendEmailVerification().then(function() {
+    // Email sent.
+    window.alert("email sent");
+  }).catch(function(error) {
+    // An error happened.
+    window.alert("email error + " + error.message);
+  });
+}
+
+function facebook_login() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+
+  window.alert("login with g" + user.email);
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+
+    window.alert("error mess + " + error.message + " user email " + email);
     // ...
   });
 }
